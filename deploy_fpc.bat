99aef63201966cb34a8ef01a845720be1121fbe0
temcdrm <t.mcdermott@ieee.org>
2023-10-09 16:36:14 -0400

    archiving both fpc and delphi dll for Windows

--- deploy_fpc.bat
+++ deploy_fpc.bat
@@ -1,1 +1,2 @@
-copy \src\opendss\version8\source\cmd\test\opendssdirect.dll src\py_dss_interface\dll\x64
+copy \src\opendss\version8\source\cmd\test\opendssdirect.dll src\py_dss_interface\dll\x64\fpc
+copy src\py_dss_interface\dll\x64\fpc\opendssdirect.dll src\py_dss_interface\dll\x64
